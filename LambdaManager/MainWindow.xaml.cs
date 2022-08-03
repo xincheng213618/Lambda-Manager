c3d577e82c001c2a3b4726e964fb987434665116
xincheng <xincheng213618@gmail.com>
2022-08-03 22:41:32 +0800

    删除掉多余的主窗口

--- LambdaManager/MainWindow.xaml.cs
+++ LambdaManager/MainWindow.xaml.cs
@@ -24,13 +24,11 @@ namespace LambdaManager
         public TabControl tabControl;
 
         public List<int> ClosingViewIndex = Common.ClosingViewIndex;
-        internal string? Notice { get; set; }
         internal bool Maximize { get; set; }
         internal bool IsLeftViewHidden { get; set; }
         internal bool IsMiddleViewHidden { get; set; }
         internal double LeftViewWidth { get; set; }
         internal double MiddleViewWidth { get; set; }
-
 
         public MainWindow()
         {
@@ -110,7 +108,6 @@ namespace LambdaManager
                     view.State = ViewState.CLOSED;
                 }
             }
-            //Thread.Sleep(1000);
         }
 
         private void Window_ContentRendered(object sender, EventArgs e)
@@ -120,13 +117,9 @@ namespace LambdaManager
             {
                 base.WindowState = WindowState.Maximized;
             }
-            if (Notice != null)
-            {
-                MessageBox.Show(Notice, LambdaManager.Properties.Resources.Notice, MessageBoxButton.OK, MessageBoxImage.Exclamation);
-            }
         }
 
-        internal Panel GetConfigPanel(Side side)
+        public Panel GetConfigPanel(Side side)
         {
             return side switch
             {
@@ -142,21 +135,12 @@ namespace LambdaManager
         }
 
 
-
-
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             //stageAcquisition.Width = SliderAll1.Value;
-            //stageAcquisition.Height = SliderAll1.Value * (this.Height - 48) / (this.Width);
         }
 
 
-
-        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            LambdaControl.Trigger("GLOBAL_ZOOM", sender, e);
-
-        }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
@@ -218,116 +202,21 @@ namespace LambdaManager
             multiChannel = !multiChannel;
         }
 
-        private void Button_Click_5(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("START_CLICKED", sender, e);
-        }
-
-        private void Button_Click_6(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("STOP_CLICKED", sender, e);
-        }
-
-        private void Button_Click_7(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("FORWARD_CLICKED", sender, e);
-        }
-
-        private void Button_Click_8(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("BACKWARD_CLICKED", sender, e);
-        }
-
-        private void fpsState_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            LambdaControl.Trigger("FPS_CHANGED", sender, e);
-        }
-
-        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            Slider slider = sender as Slider;
-            LambdaControl.Trigger("TSERIES_CHANGED", sender, new Dictionary<string, object>() { { "num", (int)slider.Value - 1 } });
-        }
-        private void Slider_ValueChanged_2(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            Slider slider = sender as Slider;
-            LambdaControl.Trigger("ZINDEX_CHANGED", sender, new Dictionary<string, object>() { { "num", (int)slider.Value - 1 } });
-        }
-
-        private void Button_Click_9(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("QUATER_CLICKED", sender, e);
-        }
-
-        private void Button_Click_10(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("DUAL_CLICKED", sender, e);
-        }
-
-        private void Button_Click_11(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("BF_CLICKED", sender, e);
-        }
 
         private void Button_Click_12(object sender, RoutedEventArgs e)
         {
             LambdaControl.Trigger("DF_CLICKED", sender, e);
         }
 
-        private void Button_Click_13(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("RI_CLICKED", sender, e);
-        }
 
-        private void Button_Click_14(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("DP_CLICKED", sender, e);
-        }
 
-        private void Button_Click_15(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("PHI_RESETED", sender, e);
-        }
-
-        private void Button_Click_16(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("FL_CLICKED", sender, e);
-        }
-
-        private void Button_Click_17(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("3D_CLICKED", sender, e);
-        }
-
-        private void Button_Click_18(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("CUBE_CLICKED", sender, e);
-        }
-
-        private void Button_Click_19(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("REPO_CLICKED", sender, e);
-        }
-
-        private void Button_Click_20(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("POINTER_CLICKED", sender, e);
-        }
 
         private void Button_Click_21(object sender, RoutedEventArgs e)
         {
             LambdaControl.Trigger("SELECT_CLICKED", sender, e);
         }
 
-        private void Button_Click_22(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("MOVE_CLICKED", sender, e);
-        }
 
-        private void Button_Click_23(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("SEARCH_CLICKED", sender, e);
-        }
 
         private void Button_Click_24(object sender, RoutedEventArgs e)
         {
@@ -339,90 +228,14 @@ namespace LambdaManager
             LambdaControl.Trigger("ZOME_OUT_CLICKED", sender, e);
         }
 
-        private void Button_Click_26(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("SCALE_RESETED", sender, e);
-        }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            LambdaControl.Trigger("SCALE_CHANGED", sender, e);
-        }
 
-        private void Button_Click_27(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("DIMENSION_CLICKED", sender, e);
-        }
-
-        private void Button_Click_28(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("FOCUS_CLICKED", sender, e);
-        }
-
-        private void Button_Click_29(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("RULER_CLICKED", sender, e);
-        }
-
-        private void Button_Click_30(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("ERASER_CLICKED", sender, e);
-        }
-
-        private void Button_Click_31(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("TEXT_CLICKED", sender, e);
-        }
-
-        private void Button_Click_32(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("ARROW_CLICKED", sender, e);
-        }
-
-        private void Button_Click_33(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("LINE_CLICKED", sender, e);
-        }
-
-        private void Button_Click_34(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("CURVE_CLICKED", sender, e);
-        }
-
-        private void Button_Click_35(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("CIRCLE_CLICKED", sender, e);
-        }
-
-        private void Button_Click_36(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("RECTANGLE_CLICKED", sender, e);
-        }
-
-        private void Button_Click_37(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("POLYGON_CLICKED", sender, e);
-        }
 
         private void Button_Click_38(object sender, RoutedEventArgs e)
         {
             LambdaControl.Trigger("COLORBAR_CLICKED", sender, e);
         }
 
-        private void Button_Click_39(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("MONO_CLICKED", sender, e);
-        }
-
-        private void Button_Click_40(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("DIVIDER_CLICKED", sender, e);
-        }
-
-        private void Button_Click_41(object sender, RoutedEventArgs e)
-        {
-            LambdaControl.Trigger("HISTOGRAM_CLICKED", sender, e);
-        }
 
         private void Button_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
@@ -499,41 +312,6 @@ namespace LambdaManager
             {
                 item.Visibility = ((!visible) ? Visibility.Collapsed : Visibility.Visible);
             }
-        }
-
-        private void LeftSplitter_DragCompleted(object sender, DragCompletedEventArgs e)
-        {
-            //LeftViewWidth = leftView.ActualWidth;
-        }
-
-        private void MiddleSplitter_DragCompleted(object sender, DragCompletedEventArgs e)
-        {
-            MiddleViewWidth = middleView.ActualWidth;
-        }
-
-        private void imagingView_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            //MessageBox.Show("1111");
-        }
-
-        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ToggleButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ToggleButton_Click_1(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 
